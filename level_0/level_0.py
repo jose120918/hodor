07d4c18157c4a84d418d83595ce12fefc069a57e
jose120918 <jose.esteban0918@gmail.com>
2021-01-17 22:25:12 -0500

    Primer entonto, recursos web

--- level_0/level_0.py
+++ level_0/level_0.py
@@ -2,3 +2,20 @@
 import requests
 import multiprocessing
 """ Web scraping: Program that votes 1024 times """
+
+url = 'http://158.69.76.135/level0.php'
+my_id = '1850'
+params = {'id': my_id, 'holdthedoor': 'Submit'}
+vote_success = 'Hold the Door challenge - Level 0'
+r = requests.get(url)
+
+vote = 1
+while vote <= 1024:
+    try:
+        r = requests.post(url, data=params)
+        if r.status_code is 200 and vote_success in r.text:
+            print("Upload vote #{}".format(vote), end='\r', flush=True)
+            vote += 1
+    except Exception as error:
+        print(error)
+print(":)")
